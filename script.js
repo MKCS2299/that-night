41777a38034dee2567f3fe8d4c64913953fd51fc
MKCS2299 <98272921+MKCS2299@users.noreply.github.com>
2026-02-10 20:26:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+const fox = document.getElementById("fox");
+const btn = document.getElementById("teachBtn");
+const text = document.getElementById("text");
+
+let step = 0;
+
+btn.addEventListener("click", () => {
+  step++;
+
+  if (step === 1) {
+    text.innerHTML = "Okay… arms closer?";
+    fox.classList.remove("nervous");
+    fox.classList.add("hugging");
+    btn.innerText = "Hold a little longer";
+  }
+
+  if (step === 2) {
+    text.innerHTML = "I think I got it.";
+    btn.innerText = "Your turn";
+  }
+
+  if (step === 3) {
+    text.innerHTML = 
+      "Hug bhi aata hai.<br>Aur hag toh deta hi hoon.";
+    fox.classList.add("big-hug");
+    btn.style.display = "none";
+  }
+});
